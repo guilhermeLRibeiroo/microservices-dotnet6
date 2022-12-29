3e94456ea23f1c6674fc77a4dfdb4b21d22674db
Guilherme Lazari Ribeiro <guilazribeiro@gmail.com>
2022-12-29 10:25:59 -0300

    Added product repository interface

---
+++ microservices-dotnet6/Shopping.ProductAPI/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Shopping.ProductAPI.ValueObjects;
+
+namespace Shopping.ProductAPI.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IQueryable<ProductVO>> FindAll();
+        Task<ProductVO> FindById(long productId);
+        Task<ProductVO> Create(ProductVO product);
+        Task<ProductVO> Update(ProductVO product);
+        Task<bool> Delete(long productId);
+    }
+}
