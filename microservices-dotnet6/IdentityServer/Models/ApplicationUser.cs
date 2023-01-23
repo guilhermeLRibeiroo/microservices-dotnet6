eae4cbe305c062d61d0f2968921e4b60813d07ac
Guilherme Lazari Ribeiro <guilazribeiro@gmail.com>
2023-01-23 20:23:32 -0300

    Added duende software template of identity server

---
+++ microservices-dotnet6/IdentityServer/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityServer.Models
+{
+    public class ApplicationUser
+        : IdentityUser
+    {
+        private string? FirstName { get; set; }
+        private string? LastName { get; set; }
+    }
+}
---
+++ microservices-dotnet6/IdentityServer/Models/Context/MySQLContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServer.Models.Context
+{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public class MySQLContext
+        : IdentityDbContext<ApplicationUser>
+    {
+        public MySQLContext() { }
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+    }
+}
