8efc2b75f846aaa55ad74e019fcf3582aa694347
Guilherme Lazari Ribeiro <guilazribeiro@gmail.com>
2023-06-07 20:09:14 -0300

    Fix: If the user doesn't fill in the 'Coupon Code' field, the application would crash because a value was expected in the view model of the Cart API

--- microservices-dotnet6/Shopping.CartAPI/Messages/CheckoutHeaderVO.cs
+++ microservices-dotnet6/Shopping.CartAPI/Messages/CheckoutHeaderVO.cs
@@ -7,7 +7,7 @@ namespace Shopping.CartAPI.Messages
         : BaseMessage
     {
         public string UserId { get; set; }
-        public string CouponCode { get; set; }
+        public string? CouponCode { get; set; }
         public decimal PurchaseAmount { get; set; }
         public decimal DiscountAmount { get; set; }
         public string FirstName { get; set; }
